c0bb568a87a6c3103e7fcf0d81af7eb9e8b58bfd
crystal-butler <crystal@crystal-butler.com>
2019-08-24 17:01:02 -0400

    ToH-tutorial change heroes data to art

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,11 @@
 export class Hero {
   id: number;
   name: string;
+  price: number;
+  image: string;
+  artist: string;
+  gallery: string;
+  yearmade: number;
+  yearbought: number;
+  medium: string;
 }
