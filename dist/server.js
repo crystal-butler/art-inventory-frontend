f503ef06a5b1b86a7516cfe008f9c085e39020d1
Crystal Butler <crystal.butler@arup.com>
2020-01-05 21:44:12 -0500

    add express server to frontend build directory

---
+++ dist/server.js
@@ -0,0 +1,16 @@
+/* jshint esversion: 6 */
+
+const express = require('express');
+const http = require('http');
+const app = express();
+const port = process.env.port || 4200;
+
+app.use(express.static(__dirname));
+
+// Redirect all routes to the angular app
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Frontend server running on port ${port}`));
